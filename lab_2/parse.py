c45107d9c25ce17545fe840e0cf2e966beafcd9d
asummerville <ads3pu@virginia.edu>
2021-09-19 20:49:54 -0400

    update

--- lab_2/parse.py
+++ lab_2/parse.py
@@ -48,7 +48,7 @@ def parse():
         except:
             df = df.append({'username':username, 'sesame':null, 'rps':null, 'sleep':null}, ignore_index=True)
         
-        df
+        return df
 
 if __name__ == '__main__':
     parse()
